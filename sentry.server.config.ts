32ccc06dd6b66f09e462bb1b1e17a551e330f456
Prasad Amol Kulkarni <114591944+prasadkulkarni9@users.noreply.github.com>
2026-05-14 14:35:48 +0530

    Add files via upload

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5f4435743b0dc721f384e6c21306a1f4@o4508285660168192.ingest.de.sentry.io/4508285663838288",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
